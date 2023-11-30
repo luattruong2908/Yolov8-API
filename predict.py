d4cbcb3a45695563e19a2bbb1291f2c020df4fd3
truongluat <luattruong2908@gmail.com>
2023-11-30 20:07:26 +0700

    config file

--- predict.py
+++ predict.py
@@ -1,8 +1,8 @@
 from ultralytics import YOLO
+from fastapi import FastAPI, UploadFile, File
+from fastapi.responses import JSONResponse
+from typing import List
 
-model = YOLO("pretrained/yolov8n.pt")
-
-#def predict(source):
-result = model.predict(source="1000_F_498170577_QlUqnkLW7vb4Ho5XmjgVTL6J1bMfBw8a.jpg", save=True, project="results", name="test")
-    
-#predict("shops-restaurants-around-yongkang-street-taipei-taiwan-dec-has-been-voted-one-coolest-streets-world-266468410.jpg")
+def predict(source, model, threshold):
+    model = model
+    results = model.predict(source=source, conf=threshold, project="results", name="predict")
