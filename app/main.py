ad98b9474e6d4450139ba902ddbdd4e434fc41e9
truongluat <luattruong2908@gmail.com>
2023-12-05 13:07:55 +0700

    update 5/12

--- app/main.py
+++ app/main.py
@@ -1,4 +1,25 @@
-from predict import predict
+import uvicorn
+import os
+from actions import predict
+from fastapi import FastAPI, UploadFile
+from typing import List
+
+app = FastAPI()
+
+@app.post("/actions/predictImage")
+async def predict_image(images: List[UploadFile] = UploadFile(...)):
+    os.makedirs("data/uploads", exist_ok=True)
+
+    for id, image in enumerate(images, start=1):
+        file_path = os.path.join("data/uploads", image.filename)
+
+        with open(file_path, "wb") as f:
+            f.write(image.file.read())
+
+        predict(source=file_path, model_name="models/pretrained/yolov8n.pt", threshold=0.5)
+        print(f"Image {id}/{len(images)}: Processed")
+
+    return "SUCCESS"
 
 if __name__ == "__main__":
-    print(predict(source="D:\Projects\yolo_v8\sources\street_2.jpeg", model_name="models/pretrained/yolov8n.pt", threshold=0.5))
+    uvicorn.run(app, host="localhost", port=5000)
