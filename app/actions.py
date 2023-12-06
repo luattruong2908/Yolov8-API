b18e508de4f50fe18506f251e07fd61a8e43951d
truongluat <luattruong2908@gmail.com>
2023-12-06 12:08:36 +0700

    add video_predict

--- app/actions.py
+++ app/actions.py
@@ -1,6 +1,11 @@
 from ultralytics import YOLO
 
-def predict(source, model_name="models/pretrained/yolov8n.pt", threshold=0.25, project="data", name="results"):
+def predict_image_api(source, model_name="models/pretrained/yolov8n.pt", threshold=0.25, project="data/image", name="results"):
     model = YOLO(model_name)
-    results = model.predict(source=source, conf=threshold, project=project, name=name, save=True, save_txt=True, exist_ok=True, imgsz=640, stream_buffer=True)
+    results = model.predict(source=source, conf=threshold, project=project, name=name, save=True, save_txt=True, exist_ok=True, imgsz=640)
+    return results
+
+def predict_video_api(source, model_name="models/pretrained/yolov8n.pt", threshold=0.25, project="data/video", name="results"):
+    model = YOLO(model_name)
+    results = model.predict(source=source, conf=threshold, project=project, name=name, save=True, save_txt=True, exist_ok=True, imgsz=480, stream=False)  
     return results
--- app/main.py
+++ app/main.py
@@ -1,6 +1,6 @@
 import uvicorn
 import os
-from actions import predict
+from actions import predict_image_api, predict_video_api
 from fastapi import FastAPI, UploadFile
 from typing import List
 
@@ -8,16 +8,29 @@ app = FastAPI()
 
 @app.post("/actions/predictImage")
 async def predict_image(images: List[UploadFile] = UploadFile(...)):
-    os.makedirs("data/uploads", exist_ok=True)
+    os.makedirs("data/image/uploads", exist_ok=True)
 
     for id, image in enumerate(images, start=1):
-        file_path = os.path.join("data/uploads", image.filename)
+        file_path = os.path.join("data/image/uploads", image.filename)
 
         with open(file_path, "wb") as f:
             f.write(image.file.read())
 
-        predict(source=file_path, model_name="models/pretrained/yolov8n.pt", threshold=0.25)
+        predict_image_api(source=file_path, model_name="models/pretrained/yolov8n.pt", threshold=0.25)
         print(f"Image {id}/{len(images)}: Processed")
+
+    return "SUCCESS"
+
+
+@app.post("/actions/predictVideo")
+async def predict_video(video: UploadFile = UploadFile(...)):
+    os.makedirs("data/video/uploads", exist_ok=True)
+    file_path = os.path.join("data/video/uploads", video.filename)
+
+    with open(file_path, "wb") as f:
+        f.write(video.file.read())
+
+    predict_video_api(source=file_path, model_name="models/pretrained/yolov8n.pt", threshold=0.25)
 
     return "SUCCESS"
 
