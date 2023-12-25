64079da8d0a1c9dafc20aca639b9e2cb3fe51a08
truongluat <luattruong2908@gmail.com>
2023-12-25 17:56:47 +0700

    update code 25/12

--- app/actions.py
+++ app/actions.py
@@ -1,5 +1,13 @@
 from ultralytics import YOLO
 
+def allowed_file(filename, type):
+    ALLOWED_IMAGE = {'png', 'jpg', 'jpeg'}
+    ALLOWED_VIDEO = {'mp4', 'avi', 'mov'}
+    if type == 'image':
+        return '.' in filename and filename.rsplit('.', 1)[1].lower() in ALLOWED_IMAGE
+    if type == 'video':
+        return '.' in filename and filename.rsplit('.', 1)[1].lower() in ALLOWED_VIDEO
+
 def predict_image_api(source, model_name="models/pretrained/yolov8n.pt", threshold=0.25, project="data/image", name="results"):
     model = YOLO(model_name)
     results = model.predict(source=source, conf=threshold, project=project, name=name, save=True, save_txt=True, exist_ok=True, imgsz=640)
--- app/main.py
+++ app/main.py
@@ -1,7 +1,7 @@
 import uvicorn
 import os
-from actions import predict_image_api, predict_video_api
-from fastapi import FastAPI, UploadFile
+from actions import predict_image_api, predict_video_api, allowed_file
+from fastapi import FastAPI, UploadFile, HTTPException
 from typing import List
 
 app = FastAPI()
@@ -11,6 +11,9 @@ async def predict_image(images: List[UploadFile] = UploadFile(...)):
     os.makedirs("data/image/uploads", exist_ok=True)
 
     for id, image in enumerate(images, start=1):
+        if not allowed_file(image.filename, type='image'):
+            raise HTTPException(status_code=400, detail="Wrong file type.")
+
         file_path = os.path.join("data/image/uploads", image.filename)
 
         with open(file_path, "wb") as f:
@@ -21,18 +24,38 @@ async def predict_image(images: List[UploadFile] = UploadFile(...)):
 
     return "SUCCESS"
 
-
 @app.post("/actions/predictVideo")
 async def predict_video(video: UploadFile = UploadFile(...)):
     os.makedirs("data/video/uploads", exist_ok=True)
+
+    if not allowed_file(video.filename, type='video'):
+            raise HTTPException(status_code=400, detail="Wrong file type.")
     file_path = os.path.join("data/video/uploads", video.filename)
 
     with open(file_path, "wb") as f:
         f.write(video.file.read())
 
-    predict_video_api(source=file_path, model_name="models/pretrained/yolov8n.pt", threshold=0.25)
+    predict_video_api(source=file_path, model_name="models/pretrained/yolov8n.pt", threshold=0.25) 
+
+    return "SUCCESS"
+
+@app.post("/actions/predictImage")
+async def predict_image(images: List[UploadFile] = UploadFile(...)):
+    os.makedirs("data/image/uploads", exist_ok=True)
+
+    for id, image in enumerate(images, start=1):
+        if not allowed_file(image.filename, type='image'):
+            raise HTTPException(status_code=400, detail="Wrong file type.")
+
+        file_path = os.path.join("data/image/uploads", image.filename)
+
+        with open(file_path, "wb") as f:
+            f.write(image.file.read())
+
+        predict_image_api(source=file_path, model_name="models/pretrained/yolov8n.pt", threshold=0.25)
+        print(f"Image {id}/{len(images)}: Processed")
 
     return "SUCCESS"
 
 if __name__ == "__main__":
-    uvicorn.run(app, host="localhost", port=5000)
+    uvicorn.run("main:app", host="localhost", port=5000, reload=True)
