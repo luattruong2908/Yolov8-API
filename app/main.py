a8c6f666a6f23c601dac261760a99753206aa161
truongluat <luattruong2908@gmail.com>
2024-01-02 17:56:09 +0700

    debug 2/1

--- app/main.py
+++ app/main.py
@@ -1,19 +1,27 @@
 import uvicorn
 import os
 from actions import predict_image_api, predict_video_api, allowed_file
-from fastapi import FastAPI, UploadFile, HTTPException, BackgroundTasks
+from fastapi import FastAPI, UploadFile, HTTPException #, BackgroundTasks
 from typing import List
 
 app = FastAPI()
 
 ml_status = False
+is_running = 0
 
-async def file_process(file_type, files):
+def file_process(file_type, files):
     os.makedirs(f"data/{file_type}/uploads", exist_ok=True)
 
-    for id, file in enumerate(files, start=1):
+    global ml_status
+    global is_running
+    ml_status = True
+    is_running += 1
 
+    for id, file in enumerate(files, start=1):
         if not allowed_file(file.filename, type=file_type):
+            ml_status = False
+            is_running -= 1
+
             raise HTTPException(status_code=400, detail="Wrong file type.")
         
         # Save file to specific location
@@ -27,14 +35,17 @@ async def file_process(file_type, files):
         elif file_type == "video":
             predict_video_api(source=file_path, model_name="models/pretrained/yolov8n.pt", threshold=0.25)
             print(f"Video {id}/{len(files)}: Processed")
+        print(is_running)
         
-        global ml_status
+    is_running -= 1
+    if is_running == 0:
         ml_status = False
 
-        print("DONE PROCESSING!!!")
+    print("DONE PROCESSING!!!")
+    return None
 
 @app.get("/")
-async def root():
+def root():
     return {"Hello": "World"}
 
 @app.get("/setup")
@@ -44,16 +55,11 @@ def setup():
     return ml_status
 
 @app.post("/actions/predict/{file_type}")
-async def predict(file_type: str, files: List[UploadFile], background_tasks: BackgroundTasks):
-    global ml_status
-    ml_status = True
-
-    async def run_file_process():
-        await file_process(file_type, files)
-    
-    background_tasks.add_task(run_file_process)
+def predict(file_type: str, files: List[UploadFile]):
+    file_process(file_type, files)
 
     return "PROCESSING..."
         
 if __name__ == "__main__":
      uvicorn.run("main:app", host="localhost", port=8000, reload=True)
+
